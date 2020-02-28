5d545d811796c46bbdb3927de8aa4ac9a6ebd7a9
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2020-02-28 14:27:17 +0700

    Updates

---
+++ precache-manifest.e172c6231ad4ea5b114ecc047ac824a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93e4b12375bba6e0dafdd00d324d19d",
+    "url": "/makemek/bof-microsite-treeview/index.html"
+  },
+  {
+    "revision": "896b51066c4664fcb84e",
+    "url": "/makemek/bof-microsite-treeview/static/css/2.29bf60af.chunk.css"
+  },
+  {
+    "revision": "ba04ab9dbaaf8ea9471e",
+    "url": "/makemek/bof-microsite-treeview/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "896b51066c4664fcb84e",
+    "url": "/makemek/bof-microsite-treeview/static/js/2.cd8c2e8c.chunk.js"
+  },
+  {
+    "revision": "1efd3a33550a4d670c5a5386e0cc5e8e",
+    "url": "/makemek/bof-microsite-treeview/static/js/2.cd8c2e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba04ab9dbaaf8ea9471e",
+    "url": "/makemek/bof-microsite-treeview/static/js/main.1ee922d3.chunk.js"
+  },
+  {
+    "revision": "8fa2e47ba37657e21503",
+    "url": "/makemek/bof-microsite-treeview/static/js/runtime-main.db2fb66a.js"
+  }
+]);
